d14b0e026224e6b66d1a0357e2d84c1163842d34
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2018-10-26 01:37:28 +0200

    add premium package route and sheet connection

--- routes/routes.js
+++ routes/routes.js
@@ -93,7 +93,58 @@ router.post('/pkg/basic', async (req, res) => {
   }
 });
 
-router.post('/pkg/premium', (req, res) => {});
+router.post('/pkg/premium', (req, res) => {
+  const {
+    sample,
+    address1,
+    address2,
+    phoneNumber,
+    mail,
+    zipCode,
+    postalRegion,
+    isBigClinic,
+    wantNewsletter,
+    contactFirstname,
+    contactLastname,
+    companyAddress1,
+    companyAddress2,
+    companyZipcode,
+    companyPostalRegion,
+    companyName
+  } = req.body.cntPkg;
+
+  try {
+    await addRowToPremiumPkg(
+      sample,
+      address1,
+      address2,
+      phoneNumber,
+      mail,
+      zipCode,
+      postalRegion,
+      isBigClinic,
+      contactFirstname,
+      contactLastname,
+      companyAddress1,
+      companyAddress2,
+      companyZipcode,
+      companyPostalRegion,
+      companyName
+    );
+    console.log('sucess inserting row to basic package sheet');
+    if (wantNewsletter === true) {
+      const fullname = contactFirstname.concat(` ${contactLastname}`);
+      await addRowToNewsletter(fullname, mail);
+      console.log('sucess adding new subscriber to newsletter sheet');
+    }
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ msg: err });
+  }
+
+  
+});
 
 router.post('/pkg/platinum', (req, res) => {});
 
---
+++ spreadsheets/pkgs/premium.js
@@ -0,0 +1,61 @@
+const GoogleSpreadsheet = require('google-spreadsheet');
+const { promisify } = require('util');
+const SPREADSHEET_ID = '14Ir2N45tHjyIVjYuAYrhIzNE_F7Yd_6yTVNz_yMYUgY';
+const { NODE_ENV } = process.env;
+
+let credentials;
+if (NODE_ENV !== 'production') {
+  credentials = require(`../../service-account.json`);
+} else {
+  credentials = {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY
+  };
+}
+
+module.exports = async (
+  sample,
+  address1,
+  address2,
+  phoneNumber,
+  mail,
+  zipCode,
+  postalRegion,
+  isBigClinic,
+  contactFirstname,
+  contactLastname,
+  companyAddress1,
+  companyAddress2,
+  companyZipcode,
+  companyPostalRegion,
+  companyName
+) => {
+  const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
+  await promisify(doc.useServiceAccountAuth)(credentials);
+  const info = await promisify(doc.getInfo)();
+  console.log(`Loaded doc: ` + info.title + ` by ` + info.author.email);
+  const sheet = info.worksheets[0];
+  console.log(
+    `sheet 1: ` + sheet.title + ` ` + sheet.rowCount + `x` + sheet.colCount
+  );
+
+  const insertNewRow = {
+    package_type: sample,
+    adress1: address1,
+    address2_optional: address2,
+    phonenumber: phoneNumber,
+    mail: mail,
+    zipcode: zipCode,
+    postal_region: postalRegion,
+    is_big: isBigClinic,
+    contact_person_firstname: contactFirstname,
+    contact_person_lastname: contactLastname,
+    companyAddress1: companyAddress1,
+    companyAddress2_optional: companyAddress2,
+    companyZipcode: companyZipcode,
+    companyPostalRegion: companyPostalRegion,
+    companyName: companyName
+  };
+  const insert = await promisify(sheet.addRow)(insertNewRow);
+  return insert;
+};
